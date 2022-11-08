8c82cc47d8bd18a5d6168eec40c874de8c11088d
thesejeans21 <117754127+thesejeans21@users.noreply.github.com>
2022-11-08 16:48:08 +0000

    Update make-plot.R

--- git-usage/src/make-plot.R
+++ git-usage/src/make-plot.R
@@ -1,22 +1,19 @@
 library(ggplot2)
-sods_data <- read.csv("stackoverflow -git-data.csv")
 g <- ggplot(
-  data = sods_data ,
-  mapping = aes(x = year, y = percentage)) + geom_point() +
-  geom_smooth(method = "lm") +
+  data = sods_data,
+  mapping = aes(x = year, y = percentage)) +
+  geom_point() +
+  geom_smooth(method = "lm", colour = "darkgrey") +
   geom_text(
-    aes(x = 2020, y = 82.8, label = "only GitHub"), nudge_x = 0.2,
-    nudge_y = -4) +
+    aes(x = 2020, y = 82.8, label = "only GitHub"),
+    size = 3,
+    nudge_x = 0.2,
+    nudge_y = -6) +
   labs(
     x = "Year",
-    y = "Percentage who used git",
-    title = "Git usage has increased",
-    subtitle = "Data from Stackoverflow Developer Survey")
-ggsave(filename = "git-usage.png", plot = g,
-       height = 7.4, width = 10.5 , units = "cm")
-sink(file = "regression -summary.txt")
-summary(lm(percentage ~ year, data = sods_data))
-sink ()
+    y = "Percentage who used git") +
+  ylim(c(0,100)) +
+  theme_bw()
 
 sink(file = "out/package-versions.txt")
 sessionInfo()
